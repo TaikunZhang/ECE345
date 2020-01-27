32d9eccb9c633f1c3122e9921b59f9a81f3edb9a
TaikunZhang <taikun.zhang@mail.utoronto.ca>
2020-01-26 21:57:17 -0500

    Assign1

--- assign1/sort2.cpp
+++ assign1/sort2.cpp
@@ -11,6 +11,8 @@
 #include <limits>
 #include <queue>
 #include <tgmath.h> 
+#include <chrono>
+using namespace std::chrono;
 using namespace std;
 
 void mergesort(vector<double>& arr, int l, int r);
@@ -64,30 +66,35 @@ void merge(vector<double>& arr, int l, int m, int r){
     }
 }
 
-int main(int argc, char* argv[])
-{
+int main(int argc, char* argv[]){
     ifstream myfile (argv[1]);
     string line,word;
     vector<double> arr;
+    //int size = 0;
+    //int limit = 25000;
     if (myfile.is_open()){
 
         while (!myfile.eof()){
             getline (myfile,line);
             stringstream s(line); 
             int count = 0;
-            while (getline(s, word, ',') && count < 1) { 
+            while (getline(s, word, ',') && count < 1 /*&& size <= limit*/) { 
                 int k = stoi(word);
                 arr.push_back(k);
                 count++; 
+                //size++;
             }
         }
             myfile.close();
     } 
-
+    //high_resolution_clock::time_point t_start = high_resolution_clock::now();
     mergesort(arr,0,arr.size()-1);
-
+    //high_resolution_clock::time_point t_end = high_resolution_clock::now();
     for(int i = 0; i < arr.size(); ++i){
         cout << arr[i] << endl;
     }
+    //auto duration = duration_cast<nanoseconds>(t_end-t_start).count();
+    //cout << duration << endl;
+    //cout << arr.size() << endl;
     return 0;
 }
---
+++ assign1/sort3.cpp
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: mahroos2
+ *
+ * Created on January 19, 2020, 2:38 PM
+ */
+
+
+
+#include <cstdlib>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <algorithm>
+#include <set>
+#include <limits>
+#include <chrono>
+using namespace std::chrono;
+#include <bits/stdc++.h> 
+
+using namespace std;
+
+
+void bubbleSort(vector<double>& arr, int n)  
+{  
+    int i, j;
+    for (i = 0; i < n-1; i++){
+        for (j = 0; j < n-i-1; j++){
+            
+            if (arr[j] > arr[j+1]){
+                double temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}  
+
+
+
+void cocktailSort(vector<double>& arr, int n)
+{ 
+    bool swapped = true;
+    int start = 0;
+    int end = n - 1;
+ 
+    while (swapped) {
+        swapped = false;
+  
+        for (int i = start; i < end; ++i) {
+            if (arr[i] > arr[i + 1]) {
+                swap(arr[i], arr[i + 1]);
+                swapped = true;
+            }
+        }
+        if (!swapped)
+            break;
+  
+        swapped = false;
+        --end;
+        
+        for (int i = end - 1; i >= start; --i) {
+            if (arr[i] > arr[i + 1]) {
+                double temp = arr[i];
+                arr[i] = arr[i+1];
+                arr[i+1] = temp;
+                swapped = true;
+            }
+        }
+        ++start;
+    }
+}
+  
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    ifstream myfile (argv[1]);
+    string line,word;
+    vector<double> arr;
+    //int size = 0;
+    //int limit = 25000;
+    if (myfile.is_open()){
+
+        while (!myfile.eof()){
+            getline (myfile,line);
+            stringstream s(line); 
+            int count = 0;
+            while (getline(s, word, ',') && count < 1 /*&& size <= limit*/) { 
+                int k = stoi(word);
+                arr.push_back(k);
+                count++; 
+                //size++;
+            }
+        }
+            myfile.close();
+    } 
+    //high_resolution_clock::time_point t_start = high_resolution_clock::now();
+    int n = arr.size();
+    bubbleSort(arr, n);
+    //cocktailSort(arr, n);
+    //high_resolution_clock::time_point t_end = high_resolution_clock::now();
+    for(int i = 0; i< arr.size(); ++i)
+        cout << arr[i] << endl;
+
+    //auto duration = duration_cast<nanoseconds>(t_end-t_start).count();
+    //cout << duration << endl;
+    //cout << arr.size() << endl;
+    return 0;
+}
+
